a8694ef806a1e47cc623f9bcb43d490ae87c3bb6
martin <martin.penard@gmail.com>
2018-05-15 22:59:28 +0200

    finished mesh files -- need to setup assimp import

--- src/graphics/mesh.cpp
+++ src/graphics/mesh.cpp
@@ -42,7 +42,21 @@ namespace rain
 		unsigned int specularNr = 1;
 
 		// TODO
+		for (unsigned int i = 0; i < textures.size(); i++)
+		{
+			glActiveTexture(GL_TEXTURE0 + i); // activate proper texture unit before binding
+											  // retrieve texture number (the N in diffuse_textureN)
+			std::string number;
+			std::string name = textures[i].type;
+			if (name == "texture_diffuse")
+				number = std::to_string(diffuseNr++);
+			else if (name == "texture_specular")
+				number = std::to_string(specularNr++);
 
+			_shader->setParameter(("material." + name + number).c_str(), (int)i);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+		}
+		glActiveTexture(GL_TEXTURE0);
 
 		glBindVertexArray(m_vao);
 		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
