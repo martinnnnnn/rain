7f7d2e7533841371e4fdbefef596288f131a3afc
penard <martin.penard@gmail.com>
2018-08-24 15:54:33 +0200

    shder fix

--- resources/engine/dx12/shaders/vertex/vertex_shader.hlsl
+++ resources/engine/dx12/shaders/vertex/vertex_shader.hlsl
@@ -24,7 +24,7 @@ VertexShaderOutput main(VertexPosColor IN)
 {
     VertexShaderOutput OUT;
  
-    OUT.Position = mul(ModelViewProjectionCB, float4(IN.Position, 1.0f));
+    OUT.Position = mul(ModelViewProjectionCB.MVP, float4(IN.Position, 1.0f));
     OUT.Color = float4(IN.Color, 1.0f);
  
     return OUT;
