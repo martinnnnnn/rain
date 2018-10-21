8bb35c3cf441f4beff3d29a3bd60fbe6edabbf8a
martinnnnnn <martin.penard@gmail.com>
2018-10-21 10:02:45 +0200

    add sphere rendering

--- src/gfx/ogl/ogl_renderer.h
+++ src/gfx/ogl/ogl_renderer.h
@@ -7,6 +7,8 @@
 #include "gl/GLU.h"
 #include "glm/glm.hpp"
 
+#include "core/types.h"
+
 class Shader;
 
 struct StaticMesh
@@ -20,22 +22,33 @@ public:
     void init();
 
     void initialize_coord_view();
+    void render_coord_view(const glm::vec3& _position);
+
     void initialize_quad();
-    void initialize_cube();
+    void render_quad();
+    
+	void initialize_cube();
+    void render_cube(const glm::vec3& _position);
+
+	void init_sphere();
+	void render_sphere(const glm::vec3& _position);
 
     void clear();
     void set_projection_matrix(const glm::mat4& _projection);
     void set_view_matrix(const glm::vec3& _eye, float _pitch, float _yaw);
     void set_view_matrix(const glm::vec3& _eye, const glm::vec3& _center, const glm::vec3& _up);
     void set_view_matrix(const glm::mat4& _matrix);
-    void render_quad();
-    void render_coord_view(const glm::vec3& _position);
-    void render_cube(const glm::vec3& _position);
     void draw_triangle();
 
     GLuint coordviewVAO;
-    GLuint quadVAO;
-    GLuint cubeVAO;
+	u32 coord_index_count;
+	GLuint quadVAO;
+	u32 quad_index_count;
+	GLuint cubeVAO;
+	u32 cube_index_count;
+	GLuint sphereVAO;
+	u32 sphere_index_count;
+
     unsigned int cubeShaderProgram;
     unsigned int coordviewShaderProgram;
     glm::mat4 projection;
