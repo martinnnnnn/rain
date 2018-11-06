e752b43bf8a76f52827e651e5963297eec778568
penard <martin.penard@gmail.com>
2018-11-06 11:03:52 +0100

    scaling both angular momentum and spin with time, seems better but size/mass pairs are very limited because - i think - the method to get inertia from them isnt really relyable for bigger values

--- src/physics/rigid_body.cpp
+++ src/physics/rigid_body.cpp
@@ -39,24 +39,28 @@ namespace rain
     {
         _body.orientation = _initialRotation;
         _body.size = 1.0f;
-        _body.mass = 1.6f;
+        _body.mass = 1.0f;
         _body.mass_inverse = 1.0f / _body.mass;
         _body.rotationInertia = powf((1.0f / 6.0f) * _body.size, 2.0f * _body.mass);
-        _body.rotationInertiaInverse = 1.0f * _body.rotationInertia;
+        _body.rotationInertiaInverse = 1.0f / _body.rotationInertia;
+
+        char buffer[512];
+        sprintf_s(buffer, "inertia : %f\ninertia inverse : %f\n\n", _body.rotationInertia, _body.rotationInertiaInverse);
+        OutputDebugStringA(buffer);
     }
 
     void update_orientation(RigidBodyOrientation& _body, float _deltaTime)
     {
-        _body.torque = (glm::vec3(0.0f, 0.0f, -1.0f) - _body.angularVelocity * 0.1f)* _deltaTime;
+        _body.torque = (glm::vec3(0.0f, 0.0f, -1.0f) - _body.angularVelocity * 0.2f);
         
         char buffer[512];
         sprintf_s(buffer, "velocity : (%f,%f,%f)\n", _body.angularVelocity.x, _body.angularVelocity.y, _body.angularVelocity.z);
         OutputDebugStringA(buffer);
 
-        _body.angularMomentum += _body.torque;
+        _body.angularMomentum += _body.torque * _deltaTime;
         _body.angularVelocity = _body.angularMomentum * _body.rotationInertiaInverse;
         _body.spin = 0.5f * glm::quat(0, _body.angularVelocity.x, _body.angularVelocity.y, _body.angularVelocity.z) * _body.orientation;
-        _body.orientation += _body.spin;
+        _body.orientation += _body.spin * _deltaTime;
         _body.orientation = glm::normalize(_body.orientation);
     }
 
